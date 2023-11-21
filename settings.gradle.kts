e411f7a2f37e18934c96a4dffbc841c385ed515f
yp897 <iubulavkin@yandex.ru>
2023-11-21 23:36:40 +0300

    lib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,14 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://jitpack.io")
         }
+        maven {
+            url = uri("https://jitpack.io")
+            content { includeGroup("com.github.GoodAppR") }
+        }
+        maven {
+            url = uri("https://github.com/GoodAppR/Custom-Toast")
+            content { excludeGroupByRegex("com\\.github.GoodAppR.*") }
+        }
     }
 }
 
